69da775afeebf04ecfe77bd365b0f94b20b8e735
AndreRongcales19 <302334829@student.rocmondriaan.nl>
2023-11-14 12:08:57 +0100

    .

--- js/main.js
+++ js/main.js
@@ -21,33 +21,46 @@ raise = bag * percent;
 let bagWithRaise = bag + raise; 
 container.innerHTML += `<li>${bagWithRaise}</li>`;
 
-//opdr 4 
-let person = prompt("Wat ben jij?"); 
-if (person === "docent") {
-    console.log("Jij bent een docent!");
-} else if (person === "student") {
-    console.log("Jij bent een student!");
-} else if (person === "man") {
-    console.log("Jij bent een man!");
-} else if (person === "vrouw") {
-    console.log("Jij bent een vrouw!");
-} else {
-    console.log("Jij bent Gek");
-}
+// //opdr 4 
+// let person = prompt("Wat ben jij?"); 
+// if (person === "docent") {
+//     console.log("Jij bent een docent!");
+// } else if (person === "student") {
+//     console.log("Jij bent een student!");
+// } else if (person === "man") {
+//     console.log("Jij bent een man!");
+// } else if (person === "vrouw") {
+//     console.log("Jij bent een vrouw!");
+// } else {
+//     console.log("Jij bent Gek");
+// }
 
 //opd 5 
-const excercise5 = document.querySelector("excercise5"); 
+const excercise5 = document.querySelector("excercise-5"); 
 const btnCalculator = document.querySelector(".calculator");
+
 btnCalculator.addEventListener("click", function(){
     const askDollars = parseInt(prompt("Hoeveel dollars heb jij?"));
     const askCrypto = prompt("welke crypto kies jij");
-    if (askCrypto === "bitcoin") {
-        let result = askDollars / 35050;
-        excercise5.innerHTML = result; 
-    } else if (askCrypto === "ethereum") {
-        result = askDollars / 1887; 
-        excercise5.innerHTML = result; 
+    if (askCrypto == "bitcoin") {
+        const valueBitcoin = askDollars / 35050;
+        excercise5.innerHTML = valueBitcoin; 
+    } else if (askCrypto == "ethereum") {
+        const valueEthereum = askDollars / 1887; 
+        excercise5.innerHTML = valueEthereum; 
+    } else {
+        excercise5.textContent = "hallo";
     }
 });
 
+// odpr 10 
 
+let artists = ["Ed", "Eminem", "Beyonce", "Bruno", "Justin Bieber"]; 
+const addBtn = document.querySelector(".add-item");
+const ulList = document.querySelector(".list");
+addBtn.addEventListener("click", function(){
+    for (let i = 0; i < artists.length; i++) {
+        const artist = artist[i];
+        ulList.innerHTML += artist;
+    }
+});
